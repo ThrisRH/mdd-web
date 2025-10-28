62beff3f9447a069fe639dded4fe072bdddc2dee
ThrisRH <144113163+ThrisRH@users.noreply.github.com>
2025-10-28 17:13:51 +0700

    rename components/Auth -> components/auth

--- src/app/(user)/auth/login/page.tsx
+++ src/app/(user)/auth/login/page.tsx
@@ -1,9 +1,9 @@
 "use client";
-import { AuthBodyContainer } from "../../../../components/auth/styled";
-import SignInGoogle from "../../../../components/auth/LogWithGoogle";
-import SignInStrapi from "../../../../components/auth/LogWithStrapi";
-import { FormContainer } from "../../../../styles/components/layout/Common.styles";
-import { UserInfoContextProvider } from "../../../../context/user-info-context";
+import { AuthBodyContainer } from "@/components/auth/styled";
+import SignInGoogle from "@/components/auth/LogWithGoogle";
+import SignInStrapi from "@/components/auth/LogWithStrapi";
+import { FormContainer } from "@/styles/components/layout/Common.styles";
+import { UserInfoContextProvider } from "@/context/user-info-context";
 
 export default function LoginPage() {
   return (
